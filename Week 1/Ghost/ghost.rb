7052ec7fa0ba74bf2b0ec0597cbacadb71cfa658
App Academy Student <student@appacademy.io>
2015-10-27 08:56:57 -0700

    Work on Ghost and Player

--- Week 1/Ghost/ghost.rb
+++ Week 1/Ghost/ghost.rb
@@ -1,40 +1,39 @@
-class Game
+require_relative 'player'
 
+class GhostGame
   attr_reader :players, :dictionary
   attr_accessor :fragment
 
-  $current_player
-
-  def initialize(players, fragment, dictionary='ghost-dictionary.txt')
+  def initialize(*players)
     @players = players
-    @fragment = fragment
-    @dictionary = dictionary
-    $current_player = players.first
+    @current_player = players.first
+    @dictionary = File.readlines('ghost-dictionary.txt')
+    current_player = players.first
   end
 
   def play_round
-
+    current_player.guess(@fragment)
   end
 
   def current_player
-    $current_player
+    current_player = players.first
   end
 
   def previous_player
-    if $current_player == players.first
+    if current_player == players.first
       return players.first
     else
-      idx = players.index($current_player)
+      idx = players.index(current_player)
       return players[idx - 1]
     end
   end
 
   def next_player!
-    if $current_player == players.last
-      $current_player = players.first
+    if current_player == players.last
+      current_player = players.first
     else
-      idx = players.index($current_player)
-      $current_player = players[idx + 1]
+      idx = players.index(current_player)
+      current_player = players[idx + 1]
     end
   end
 
@@ -43,30 +42,12 @@ class Game
   end
 
   def valid_play?(str)
-    if ("a".."z").include?(str)
+    return true if ("a".."z").include?(str)
+    false
   end
-
-end
-
-class Player
-
-  attr_reader :name
-
-  def initialize(name)
-    @name = name
-  end
-
-  def guess
-
-  end
-
-  def alert_invalid_guess
-
-  end
-
 end
 
 if __FILE__ == $PROGRAM_NAME
-  g = Game.new(["Ryan", "Samp"], "tes")
+  g = GhostGame.new(Player.new("Ryan"), Player.new("Samp"))
   g.play_round
 end
