798d1cadfc23bdf71a792e47b670733882a4d723
Ryan Samp <sampryan@gmail.com>
2015-10-26 23:05:11 -0700

    Finish exercises

--- Week 1/w1d1.rb
+++ Week 1/w1d1.rb
@@ -52,21 +52,6 @@ class Array
 
   def my_flatten
     result = []
-
-    # i = 0
-    # while i < self.length
-    #   if self[i].is_a?(Array)
-    #     j = 0
-    #     el = self[i].my_flatten
-    #     while j < self[i].length
-    #       result << el[j]
-    #       j += 1
-    #     end
-    #   else
-    #     result << self[i]
-    #   end
-    #     i += 1
-    # end
     self.each do |el|
       if el.is_a?(Array)
         el.my_flatten.each do |el2|
@@ -76,6 +61,13 @@ class Array
         result << el
       end
     end
+    # self.my_each do |el|
+    #   if el.is_a?(Array)
+    #     result.concat(el.my_flatten)
+    #   else
+    #     result << el
+    #   end
+    # end
     result
   end
 
@@ -84,9 +76,9 @@ class Array
     self.each do |el|
       result << [el]
     end
-    args.each_with_index do |arg, i|
-      arg.each_with_index do |el, j|
-        result[j] << el unless result[j].nil?
+    args.each do |arg|
+      arg.each_with_index do |el, i|
+        result[i] << el unless result[i].nil?
       end
       # while result[i].length < self.length
       #   result[i] << nil
@@ -100,14 +92,38 @@ class Array
     result
   end
 
+  def my_rotate(rotation=1)
+    return self if rotation == 0
+    rotated = []
+    self.my_each { |el| rotated << el}
+    if rotation > 0
+      rotation.times { rotated.push(rotated.shift) }
+    elsif rotation < 0
+      rotation = 0 - rotation
+      rotation.times { rotated.unshift(rotated.pop) }
+    end
+    rotated
+  end
+
+  def my_join(separator="")
+    joined = ""
+    self.my_each do |el|
+      joined << el
+      joined << separator
+    end
+    joined.chomp(separator)
+  end
+
+  def my_reverse
+    reversed = []
+    self.my_each { |el| reversed.unshift(el)}
+    reversed
+  end
+
 end
 
-a = [ 4, 5, 6 ]
-b = [ 7, 8, 9 ]
-p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
-p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
-p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
-#
-c = [10, 11, 12]
-d = [13, 14, 15]
-p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
+a = [ "a", "b", "c", "d" ]
+p a.my_rotate         #=> ["b", "c", "d", "a"]
+p a.my_rotate(2)      #=> ["c", "d", "a", "b"]
+p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
+p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
