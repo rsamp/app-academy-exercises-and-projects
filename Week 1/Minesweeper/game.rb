470b826318bcfefbaecd6ac23fad2c9908cd6cb7
App Academy Student <student@appacademy.io>
2015-10-30 16:16:39 -0700

    implement game play

--- Week 1/Minesweeper/game.rb
+++ Week 1/Minesweeper/game.rb
@@ -8,10 +8,28 @@ class Game
   end
 
   def play
-    # until
+    board.render
+    until won?
+      prompt
+      spot = gets.chomp.split.map(&:to_i)
+      board[spot].reveal
       board.render
-    # end
+    end
   end
+
+  def prompt
+    puts "Tell us the spot you'd like to reveal:"
+  end
+
+  def won?
+    board.grid.each do |row|
+      row.each do |tile|
+      return false if tile.value == nil
+      end
+    end
+    true
+  end
+
 end
 
 if __FILE__ == $PROGRAM_NAME
