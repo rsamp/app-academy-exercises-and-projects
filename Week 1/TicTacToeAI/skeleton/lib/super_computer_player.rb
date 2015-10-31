9f62a6f7443a0a79e79f2bb032f1a4f33d5d1b7d
Ryan Samp <sampryan@gmail.com>
2015-10-30 18:27:53 -0700

    Test AI changes

--- Week 1/TicTacToeAI/skeleton/lib/super_computer_player.rb
+++ Week 1/TicTacToeAI/skeleton/lib/super_computer_player.rb
@@ -3,11 +3,16 @@ require_relative 'tic_tac_toe_node'
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
     root_board = TicTacToeNode.new(game.board, mark)
+    # desired_move = root_board.children.find {|child| child.winning_node?(mark)}
+    # return desired_move.prev_move_pos if desired_move
+    # desired_move = root_board.children.find {|child| !child.losing_node?(mark)}
+    # return desired_move.prev_move_pos if desired_move
+    # raise "error"
     desired_move = nil
-    root_board.children.any? do |child|
+    root_board.children.each do |child|
       if child.winning_node?(mark)
         desired_move = child.prev_move_pos
-      elsif !child.losing_node?(mark)
+      elsif !child.losing_node?(mark) && desired_move.nil?
         desired_move = child.prev_move_pos
       else
         raise "Error"
